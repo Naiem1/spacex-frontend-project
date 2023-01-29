d1cbbf29679c59736e78622232f1b4a09418d54c
Naiem Islam <naiem4288@gmail.com>
2023-01-29 15:00:12 +0600

    Fix: fixes fome bug that is nessesar for deploy

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
+import SearchBar from '../components/SearchBar/SearchBar';
 import HomePage from '../pages/homePage/HomePage';
 import LaunchDetailsPage from '../pages/LaunchDetailsPage/LaunchDetailsPage';
 
 const AppRouter: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/launches" element={<HomePage />} />
-      <Route path="/launches/:id" element={<LaunchDetailsPage />} />
-    </Routes>
+    <>
+      <SearchBar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/launches" element={<HomePage />} />
+        <Route path="/launches/:id" element={<LaunchDetailsPage />} />
+      </Routes>
+    </>
   );
 };
 
