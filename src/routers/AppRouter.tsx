4fdd43a8d5b35ecdd27a49cad90d8eae3102484c
Naiem Islam <naiem4288@gmail.com>
2023-01-29 15:16:54 +0600

    fix: fix some bug

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -3,12 +3,13 @@ import SearchBar from '../components/SearchBar/SearchBar';
 import HomePage from '../pages/homePage/HomePage';
 import LaunchDetailsPage from '../pages/LaunchDetailsPage/LaunchDetailsPage';
 
+
 const AppRouter: React.FC = () => {
   return (
     <>
       <SearchBar />
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/launches" element={<HomePage />} />
         <Route path="/launches/:id" element={<LaunchDetailsPage />} />
       </Routes>
